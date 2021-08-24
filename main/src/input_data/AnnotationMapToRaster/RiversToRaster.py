18d43566b6e551f1915c8b3c2344632fa24fa612
Rob174 <robin.128.456@gmail.com>
2021-08-24 09:16:56 -0400

    Correction river to raster to avoid memory error

--- main/src/input_data/AnnotationMapToRaster/RiversToRaster.py
+++ main/src/input_data/AnnotationMapToRaster/RiversToRaster.py
@@ -10,38 +10,53 @@ from main.src.ProgressBar.IterationManager import IterationManager
 from main.src.ProgressBar.ProgressBar0 import ProgressBar0
 from main.src.input_data.GeotiffOpener import GeotiffOpener
 from main.src.input_data.ShapefileReaders.RiversShpFileReader import RiversShpFileReader
-from rich import Progress
 
 
 class RiversToRaster:
     def __init__(self,cache_path: Optional[Union[str,Path]] = None):
         self.dico_lines_per_tiff = {}
         self.cache_path = cache_path
-        self.generate()
-        self.save()
-    def generate(self):
+    def generate(self,start:int,stop:int):
+        FolderInfos.init(test_without_data=True)
         geotiff_opener = GeotiffOpener()
         shapefile_reader = RiversShpFileReader(FolderInfos.data_raw.joinpath("rivers").joinpath("COURS_D_EAU.shp"))
-        self.dico_lines_per_tiff = {}
-        progress = ProgressBar0(IterationManager(total=len(geotiff_opener.iter_geotiff())*len(shapefile_reader.get_points())))
+        dico_lines_per_tiff = {}
+        size = stop-start+1
+        list_points = list(shapefile_reader.get_points())
+        size *= len(list_points)
+        progress = ProgressBar0(IterationManager(total=size))
         with progress:
-            for tiff in geotiff_opener.iter_geotiff():
-                self.dico_lines_per_tiff[tiff.current_path] = []
-                for points in shapefile_reader.get_points():
+            for tiff in geotiff_opener.iter_geotiff(start=start,stop=stop):
+                dico_lines_per_tiff[tiff.current_path] = []
+                for points in list_points:
                     points_transformed = []
                     for point in points:
                         points_transformed.append(tiff.loc_to_px(point))
                     if 0 in np.max(points,axis=0):
                         continue
-                    self.dico_lines_per_tiff[tiff.current_path].append(points_transformed)
+                    dico_lines_per_tiff[tiff.current_path].append(points_transformed)
                     progress.on_end()
-            return self.dico_lines_per_tiff
+        with open(FolderInfos.data_raw.joinpath("rivers").joinpath(f"from_{start}_to_{stop}.json")) as fp:
+            json.dump(dico_lines_per_tiff,fp)
+        return None
+    def len_geotiff(self):
+        return len(GeotiffOpener())
     def save(self):
         if self.cache_path is None:
             return
         with open(self.cache_path,"w") as fp:
             json.dump(self.dico_lines_per_tiff,fp)
 
+import multiprocessing as mp
 if __name__ == '__main__':
+    num_cpu = mp.cpu_count()//2
+    pool = mp.Pool(num_cpu)
     FolderInfos.init(test_without_data=True)
-    r = RiversToRaster(cache_path=FolderInfos.data_raw.joinpath("rivers").joinpath("cache_rivers.json"))
+    r = RiversToRaster()
+    nb_elem = r.len_geotiff()
+    split_ids = [int(v) for v in np.linspace(0,nb_elem,num_cpu*16)]
+
+    results = [pool.apply(RiversToRaster().generate, args=(start,stop)) for start,stop in zip(split_ids[:-1],split_ids[1:])]
+    pool.close()
+    s=0
+
--- main/src/input_data/GeotiffOpener.py
+++ main/src/input_data/GeotiffOpener.py
@@ -10,11 +10,15 @@ class GeotiffOpener:
         self.current_rio_object = None
         self.current_path = None
 
-    def iter_geotiff(self):
-        for path in self.base_path.iterdir():
+    def iter_geotiff(self,start:int,stop:int):
+        assert type(start) is int, f"start is not int but {type(start)}"
+        assert type(stop) is int, f"stop is not int but {type(stop)}"
+        pathes = list(self.base_path.iterdir())
+        for path in pathes[start:stop]:
             self.current_rio_object = rio.open(path)
             self.current_path = path
             yield self
-
+    def __len__(self):
+        return len(list(self.base_path.iterdir()))
     def loc_to_px(self, point: Tuple[float, float]):
         return self.current_rio_object.index(*point)
